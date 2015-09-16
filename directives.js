1c5362d270f46c0e422a14eaa7983a2af7878122
fishineyuan <382184760@qq.com>
2015-09-16 13:19:53 +0800

    add support

--- directives.js
+++ directives.js
@@ -1,9 +1,17 @@
 
 var $ = require('cheerio');
 
+function eq(exp) {
+    return '{{= ' + exp + ' }}';
+}
+
+function tmp(exp) {
+    return 'var tmp = ' + exp + ';';
+}
+
 module.exports = {
     text: function(exp) {
-        $(this.el).html('{{= ' + exp + ' }}');
+        $(this.el).html(eq(exp));
     },
     //if: function(exp) {
         //$(this.el).before('{{ if (' + exp + ') { }}');
@@ -15,30 +23,46 @@ module.exports = {
     //},
     show: function (exp) {
         var tpl = '{{ if (' + exp + ') { }} display: block; {{ } }}';
-        $(this.el).css(this.strings(tpl), '0');
+        $(this.el).css(this.strings(tpl), 0);
     },
     'class': function(exp) {
         var arg = this.arg;
         if (arg) {
             var tpl = '{{ if (' + exp + ') { }} ' + arg + ' {{ } }}';
             $(this.el).addClass(this.strings(tpl));
+        } else {
+            $(this.el).addClass(this.strings(eq(exp)));
         }
     },
     value: function(exp) {
         if (this.el.type === 'checkbox') {
-            $(this.el).attr('checked', '{{= ' + exp + ' }}');
+            $(this.el).attr('checked', eq(exp));
         } else {
-            $(this.el).val('{{= ' + exp + ' }}');
+            $(this.el).val(eq(exp));
         }
     },
     attr: function(exp) {
         var arg = this.arg;
         if (arg) {
-            $(this.el).attr(arg, '{{= ' + exp + ' }}');
+            if (arg !== 'style') {
+                $(this.el).attr(arg, eq(exp));
+            }
+        } else {
+            var tpl = [
+                '{{ ', 
+                    tmp(exp),
+                    'if (typeof tmp === "object" && tmp) {',
+                        'for (var k in tmp) { }}',
+                            '{{= k }}={{= tmp[k] }} ',
+                        '{{ } ',
+                    '}',
+                ' }}'
+            ].join('');
+            $(this.el).attr(this.strings(tpl), 0);
         }
     },
     src: function(exp) {
-        $(this.el).attr('src', '{{= ' + exp + ' }}');
+        $(this.el).attr('src', eq(exp));
     }
 };
 
