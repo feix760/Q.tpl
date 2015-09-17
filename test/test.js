93d996f41b00db400da279a2a252e7cd4f49f9ab
Daniel Yang <miniflycn@justany.net>
2015-09-17 15:26:31 +0800

    add test case

--- test/test.js
+++ test/test.js
@@ -1,25 +1,41 @@
-
+var tpl = require('../');
 var fs = require('fs');
-var tpl = require('../index');
+var path = require('path');
 
-var html = fs.readFileSync(__dirname + '/test.html').toString();
+function compare(caseName) {
+    var src = fs.readFileSync(path.join(__dirname, 'src', caseName + '.html'), 'utf-8');
+    var expect = fs.readFileSync(path.join(__dirname, 'expect', caseName + '.html'), 'utf-8');
+    tpl.tplCode(src).should.equal(expect);
+}
 
-//var tplCode = tpl.tplCode(html);
-//console.log(tplCode);
+describe('Q.tpl', function () {
+    it('should able to use text directive', function () {
+        compare('text');
+    });
+});
 
-var foo = tpl.compile(html);
 
-console.log(foo({
-    isShow: true,
-    imgSrc: 'http://www.baidu.com/logo.gif',
-    isRed: true,
-    pwd: '123456',
-    isCheck: false,
-    size: 35,
-    attrs: {
-        width: 50,
-        height: 100
-    }
-}));
+// var fs = require('fs');
+// var tpl = require('../index');
+
+// var html = fs.readFileSync(__dirname + '/test.html').toString();
+
+// //var tplCode = tpl.tplCode(html);
+// //console.log(tplCode);
+
+// var foo = tpl.compile(html);
+
+// console.log(foo({
+//     isShow: true,
+//     imgSrc: 'http://www.baidu.com/logo.gif',
+//     isRed: true,
+//     pwd: '123456',
+//     isCheck: false,
+//     size: 35,
+//     attrs: {
+//         width: 50,
+//         height: 100
+//     }
+// }));
 
 
---
+++ tests/test.js
@@ -0,0 +1,25 @@
+
+var fs = require('fs');
+var tpl = require('../index');
+
+var html = fs.readFileSync(__dirname + '/test.html').toString();
+
+//var tplCode = tpl.tplCode(html);
+//console.log(tplCode);
+
+var foo = tpl.compile(html);
+
+console.log(foo({
+    isShow: true,
+    imgSrc: 'http://www.baidu.com/logo.gif',
+    isRed: true,
+    pwd: '123456',
+    isCheck: false,
+    size: 35,
+    attrs: {
+        width: 50,
+        height: 100
+    }
+}));
+
+
